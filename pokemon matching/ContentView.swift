82f6c1606dac3a83f76cb1587d03752a8386a5dd
omega87910 <106021054@live.asia.edu.tw>
2020-05-09 17:02:41 +0800

    初始畫面建立

--- pokemon matching/ContentView.swift
+++ pokemon matching/ContentView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("寶可夢連連看")
+                .bold()
+                .font(.system(size: 48))
+                .foregroundColor(.yellow)
+                .padding()
+            
+            Button(action:{
+                //要執行的內容
+                print(123)
+            }){
+                //按鈕樣式設定
+                Text("開始遊戲")
+                    .padding()
+                    .font(.system(size: 26))
+                    .border(Color.blue,width:5)
+                    .cornerRadius(8)
+                    .padding()
+            }
+            
+            Button(action:{
+                print("213")
+            }){
+                Text("遊戲說明")
+                    .padding()
+                    .font(.system(size: 26))
+                    .border(Color.blue,width:5)
+                    .cornerRadius(8)
+                    .padding()
+                    
+            }
+        }.frame(minWidth:0,maxWidth: .infinity,minHeight: 0,maxHeight: .infinity)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width:896,height:414))
     }
 }
