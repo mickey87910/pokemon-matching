ece5f4304b7a500c9894e96f7a66dd5ee01c5a0b
omega87910 <106021054@live.asia.edu.tw>
2020-05-13 02:23:05 +0800

    可以建立二維陣列

--- pokemon matching/ContentView.swift
+++ pokemon matching/ContentView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var showGameView : Bool = false
+    @State var showGameView : Bool = false //轉換頁面判斷
     var body: some View {
         VStack{
             if showGameView{
@@ -43,15 +43,24 @@ struct ContentView: View {
                         .border(Color.blue,width:5)
                         .cornerRadius(8)
                         .padding()
-                    
                 }
             }
         }.frame(minWidth:0,maxWidth: .infinity,minHeight: 0,maxHeight: .infinity)
     }
 }
 struct GameView: View{
+    var pokemonClass = ["皮卡丘","噴火龍","水箭龜"]
+    var digitCounts = Array(repeating: Array(repeating:0,count:5), count: 5)//宣告一個n*n的2D陣列
     var body: some View{
-        Text("Hello World")
+        HStack{
+            ForEach(0...4,id:\.self){ i in
+                VStack{
+                    ForEach(0...4,id:\.self){ j in
+                        Text("\(self.digitCounts[i][j])")
+                    }
+                }
+            }
+        }
     }
 }
 
